951f3cf5add2c442554bc2c70f84d0edc21f84eb
Lowkey1921 <118089370+Lowkey1921@users.noreply.github.com>
2023-05-16 11:06:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const vanil = document.querySelectorAll('.vani')
+vanil.forEach((vanil) => {
+    vanil.style.fontSize = 'small'
+    vanil.style.top = '120px'
+    vanil.style.textAlign = 'center'
+    vanil.style.padding = '30px'
+    vanil.style.fontFamily = "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
+})
